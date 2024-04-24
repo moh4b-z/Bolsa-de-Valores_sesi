9bd6a195b644c8dc6659c2ba5a8b849c26747a88
moh4b-z <mohammadsalim10000@gmail.com>
2024-04-23 23:05:54 -0300

    aaa

---
+++ submenu.js
@@ -0,0 +1,10 @@
+const btnMenu = document.getElementById ('btn-menu')
+
+function activeMenu() {
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('active')
+
+}
+
+
+btnMenu.addEventListener('click', activeMenu)
